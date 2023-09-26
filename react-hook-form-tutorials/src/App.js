83ba7fd400ed23af547978326f28970ca3f07df3
Code Crafter <kumar@jatanix.com>
2023-09-26 07:42:54 +0530

    customerrormessage

--- react-hook-form-tutorials/src/App.js
+++ react-hook-form-tutorials/src/App.js
@@ -4,11 +4,13 @@ import MyForm from './Components/1_Basic_Hook_Form';
 // import MyForm1 from './Components/RHF/2_RHF_InputFields';'
 import MyForm1 from './Components/RHF/2_RHF_InputFields';
 import InputValidationForm from './Components/RHF/3_RHF_Validation';
+import CustomErrorMessage from './Components/RHF/4_RHF_CustomErrorMessage';
 
 function App() {
   return (
     <div className="App">
-      <InputValidationForm />
+      <CustomErrorMessage />
+      {/* <InputValidationForm /> */}
       {/* use MyForm1 */}
       {/* <MyForm1 /> */}
       {/* <MyForm /> */}
